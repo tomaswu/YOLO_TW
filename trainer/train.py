b303e470e13f51c7c507c49ecee46aba161b3ff2
Tomas Wu <tomaswu@qq.com>
2023-07-12 01:40:51 +0800

    backup

--- trainer/train.py
+++ trainer/train.py
@@ -94,9 +94,12 @@ class Trainer():
     def train(self):
         epoch_count = self.cfg['total_epoch']
         bz = self.cfg['batch_size']
-        self.td_loder = tud.DataLoader(dataset=self.train_data,batch_size=bz,shuffle=False,num_workers=4,drop_last=False)
+        self.td_loder = tud.DataLoader(dataset=self.train_data,batch_size=bz,shuffle=True,num_workers=0,drop_last=False)
+        dl = len(self.train_data)
         self.net.to(self.device)
+        self.loss_fn.to(self.device)
         self.net.train()
+        t0=time.time()
         for epoch in range(epoch_count):
             for count,data in enumerate(self.td_loder):
                 x,y13,y26,y52 = data
@@ -115,8 +118,10 @@ class Trainer():
                     loss26= self.loss_fn(p26,y26)
                     loss52 = self.loss_fn(p52,y52)
                     loss = loss13 + loss26 + loss52
+                    loss.backward()
                     self.optimer.step()
-                    print(f'epoch&count:{epoch}_{count},loss:{loss:.5f},loss13:{loss13:.2f},loss26:{loss26:.2f},loss52:{loss52:.2f}')
+                    print(f'{time.time()-t0:.3f}_epoch&count:{epoch}_{count}_{(count+1)*bz}/{dl},loss:{loss:.5f},loss13:{loss13:.2f},loss26:{loss26:.2f},loss52:{loss52:.2f}')
+                    t0=time.time()
                 self.summary_writer.add_scalar('loss-imgs',bz*(count+1)*(epoch+1),loss)
                 self.summary_writer.add_scalar('loss13-imgs',bz*(count+1)*(epoch+1),loss13)
                 self.summary_writer.add_scalar('loss26-imgs',bz*(count+1)*(epoch+1),loss26)
